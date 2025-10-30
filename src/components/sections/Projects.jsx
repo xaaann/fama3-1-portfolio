8551226ecd935e3f6e380445c16ebae015738bba
xaaann <roxanne.fama1@gmail.com>
2025-10-30 15:06:15 +0800

    fix typos

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -102,7 +102,7 @@ export const Projects = () => {
               </p>
 
               <div className="flex flex-wrap gap-2 mb-4">
-                {["Socket.IO", "Figma"].map((tech) => (
+                {["Figma"].map((tech) => (
                   <span
                     key={tech}
                     className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm transition hover:bg-blue-500/20 hover:-translate-y-0.5 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)]"
